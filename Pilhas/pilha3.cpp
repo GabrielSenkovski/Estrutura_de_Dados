3dfa11cc752b9663876b7e273cefd3d39541a7c0
Gabriel.Senkovski <gabrielsenkovski.capanema@gmail.com>
2026-06-17 20:33:00 -0300

    Adiciona pilha Dinamica com Template tipo T

---
+++ Pilhas/pilha3.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+template <typename T>
+
+class Pilha {
+private:
+    T* _dados;       // vetor dinamico
+    int _topo;         // indicador do topo
+    int _tam;          // indica tamanho da pilha
+
+public:
+    // Método construtor da pilha
+    Pilha(int tam) {
+        _tam = tam;
+        _dados = new T[tam];      // Aloca memoria dinamicamente
+        _topo = -1;                 // inicia vazia
+    }
+
+    void empilha(T dado){
+        if (_topo == _tam -1 )
+        {
+            throw "Erro: Pilha está cheia";
+        }
+        _topo++;
+        _dados[_topo] = dado;
+    }
+
+    T desempilha(){
+        if (_topo == -1)
+        {
+            throw "Erro: Pilha está vazia";
+        }
+        
+        //Salvando valor removido para return
+        T valorRemovido = _dados[_topo];
+        //Remove o item do topo na pilha
+        _topo--;
+        return valorRemovido;
+    }
+
+    T topo(){
+        if (_topo == -1)
+        {
+            throw "Erro: Pilha está vazia";
+        }
+        //Imprime Topo
+        return _dados[_topo];
+    }
+
+    void imprimirPilha(){
+        if (_topo == -1){
+            cout << "A pilha esta vazia";
+            return;
+        }
+        
+        for (int i = _topo; i >= 0; i--)
+        {
+            cout << "A pilha no indice " << i << " tem o valor: " << _dados[i] << "\n";
+        }
+        cout << "Portanto o Topo da Pilha final tem o valor: " <<  _dados[_topo];
+    }
+        
+
+    ~Pilha() {
+    delete[] _dados; // Libera Memória
+}
+};
+
+int main(){
+    // difinin do tamanho da pilha
+    int n;
+    cout << "Digite o Tamanho Maximo da Pilha: ";
+    cin >> n;
+    
+    //SE QUISER MUDAR O TIPO DE DADOS DA PILHA É SO ALTERAR AQUI O TIPO E NO VALOR DENTRO DO TRY
+    Pilha<int> pilha_1(n);
+    while (1)
+    {
+
+
+        // Escolher operacao
+        int operacao = 0;
+        cout << "Escolha uma Operacao: 1 - Empilha, 2 Desempilha, 3 - Imprime topo, 0 Exit e (imprime pilha inteira): ";
+        cin >> operacao;
+        
+        try {
+            // Empilhar Valor
+            if (operacao == 1)
+            {
+                int valor;
+                cout << "Insira um Valor na pilha: ";
+                cin >> valor;
+                pilha_1.empilha(valor);
+                cout << "Dado " << valor << " inserido com sucesso.\n";
+            }
+            
+            // Desempilhar valor
+            if (operacao == 2){
+                cout << "O Valor removido do topo foi: " << pilha_1.desempilha() << " \n";  
+            }
+
+            // Imprimir Topo
+            if (operacao == 3)
+            {
+                cout << "O Topo da pilha contem o valor:  " << pilha_1.topo() <<" .\n";
+            }
+            
+
+            if (operacao == 0){
+                pilha_1.imprimirPilha();
+                
+                // Condição de Parada
+                break;
+            }        
+        }
+        catch (const char* msg){
+            cout << "\n[ALERTA] " << msg << "\n\n";
+        }
+    }
+    
+    return 0;
+};
